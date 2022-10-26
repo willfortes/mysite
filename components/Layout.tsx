04eea3dfea73e382446a83ef72813c40c4057af1
willfortes <willfortes.dev@gmail.com>
2022-10-26 00:08:43 -0300

    feat: new heigth

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ type Props = {
 }
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div className="bg-dark w-screen h-screen">
+  <div className="bg-dark w-screen h-100">
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
